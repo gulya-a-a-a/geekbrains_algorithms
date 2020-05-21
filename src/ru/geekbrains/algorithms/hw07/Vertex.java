6fd351970f3c832e9a15f8a3476969e3184baa51
Aleksandr Gulyaev <adgulyaev@gmail.com>
2020-05-21 19:37:20 +0300

    Hw implementation.

--- src/ru/geekbrains/algorithms/hw07/Vertex.java
+++ src/ru/geekbrains/algorithms/hw07/Vertex.java
@@ -1,4 +1,43 @@
 package ru.geekbrains.algorithms.hw07;
 
-public class Vertex {
+public class Vertex<E> {
+    private final E mData;
+    private boolean mVisited;
+
+    private Vertex<E> mPrevious;
+
+    Vertex(E value) {
+        mData = value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    public E getData() {
+        return mData;
+    }
+
+    public boolean isVisited() {
+        return mVisited;
+    }
+
+    public void setVisited(boolean visited) {
+        mVisited = visited;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s", mData);
+    }
+
+    public void setVisited(boolean b, Vertex<E> previousNode) {
+        setVisited(b);
+        mPrevious = previousNode;
+    }
+
+    public Vertex<E> getPrevious() {
+        return mPrevious;
+    }
 }
